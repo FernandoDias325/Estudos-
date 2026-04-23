9c5d0898f1dccb727b71d6a54da7234c568733b3
Fernando Dias <fernandodias325@outlook.com.br>
2026-04-22 22:35:29 -0300

    primeiros excercicios de arrays concluido

--- Alura Node/Lógica de Programação/array/array.js
+++ Alura Node/Lógica de Programação/array/array.js
@@ -1,9 +1,26 @@
 // 1. Crie um array com 5 números e exiba o primeiro elemento
 
+const numeros = [1, 2, 3, 4, 5];
+console.log(numeros[0]);
+
 // 2. Crie um array com 5 nomes e exiba o último elemento
+
+const nome = ["João", "Maria", "Jurema", "Thay", "Sid"];
+console.log(nome[nome.length - 1]);
 
 // 3. Crie um array de números e exiba o tamanho (length)
 
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(num.length);
+
 // 4. Acesse o terceiro elemento de um array de frutas
 
+const frutas = ["banana", "Pera", "Uva", "Maça"];
+console.log(frutas[2]);
+
 // 5. Altere o segundo elemento de um array de números e exiba o array atualizado
+
+const numero = [1, 2, 3, 4, 5];
+console.log(numero);
+numero[1] = 10;
+console.log(numero);
