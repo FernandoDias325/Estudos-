5e553876539f69bd6a182cdf9ca9b18fa386d6f7
Fernando Dias <fernandodias325@outlook.com.br>
2026-04-23 11:44:04 -0300

    Fim da base sobre logica

--- Alura Node/Lógica de Programação/array/arrayFor.js
+++ Alura Node/Lógica de Programação/array/arrayFor.js
@@ -1,12 +1,42 @@
 // 1. Percorra um array de números e exiba todos os valores
+
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let i = 0; i < numeros.length; i++) {
   console.log(numeros[i]);
 }
+
 // 2. Some todos os números de um array usando for
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let soma = 0;
+for (let i = 0; i < numeros.length; i++) {
+  soma += numeros[i];
+}
+console.log(soma);
 
 // 3. Encontre o maior número de um array usando for
 
+const valor = [2, 4, 6, 10, 8, 30, 1];
+let maior = valor[0];
+for (let i = 0; i < valor.length; i++) {
+  if (valor[i] > maior) {
+    maior = valor[i];
+  }
+}
+console.log(maior);
+
 // 4. Exiba apenas os números pares de um array
 
+const n = [1, 43, 48, 98, 74, 23, 546, 48645, 54878, 34164, 131581, 13543];
+for (let i = 0; i < n.length; i++) {
+  if (n[i] % 2 === 0) {
+    console.log(n[i]);
+  }
+}
+
 // 5. Exiba cada elemento multiplicado por 2
+
+const elementos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < elementos.length; i++) {
+  console.log(elementos[i] * 2);
+}
--- Alura Node/Lógica de Programação/array/arrayForOf.js
+++ Alura Node/Lógica de Programação/array/arrayForOf.js
@@ -1,9 +1,32 @@
 // 1. Exiba todos os nomes de um array usando for...of
 
+const nomes = ["João", "Maria", "Jurema", "Thay", "Sid"];
+for (const nome of nomes) {
+  console.log(nome);
+}
+
 // 2. Exiba cada número de um array em uma nova linha
+
+const numeros = [12, 13, 14, 15, 16];
+
+for (const num of numeros) {
+  console.log(num);
+}
 
 // 3. Some todos os valores usando for...of
 
-// 4. Exiba apenas números maiores que 10
+const numeros = [12, 13, 14, 15, 16];
+let soma = 0;
+for (const num of numeros) {
+  soma += num;
+}
+console.log(soma);
 
-// 5. Conte quantos elementos existem em um array usando for...of
+// 4. Exiba apenas números maiores que 10
+const numeros = [1, 2, 3, 12, 13, 14, 15, 16];
+
+for (const maior of numeros) {
+  if (maior > 10) {
+    console.log(maior);
+  }
+}
